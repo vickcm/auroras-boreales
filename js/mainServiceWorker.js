3212eefd4b5a586367c10d8189767f1466883350
Vicky CM <77595917+vickcm@users.noreply.github.com>
2022-11-25 02:16:26 +0000

    agrega estado online y offline a la navegacion - prueba

--- js/mainServiceWorker.js
+++ js/mainServiceWorker.js
@@ -12,17 +12,17 @@ window.addEventListener('offline', event => {
 // Escucho a ver si el usuario se conecta nuevamente
 window.addEventListener('online', event => {
   console.log('usuario esta conectado!! ALEGRIA!', event);
-  estado.innerHTML = `<img class="gif" src="assets/onlinefinal.gif" alt="circulo verde representando conexión online">`
+  estado.innerHTML = `<img class="gif" src="../assets/onlinefinal.gif" alt="circulo verde representando conexión online">`
     ;
 });
 
 // Chequeo si tiene conexion al momento de carga
 if (!navigator.onLine) {
   console.log('estoy sin conexion pero en el momento de carga!!');
-  estado.innerHTML = `<img class="gif" src="assets/offlinefinal.gif" alt="circulo verde representando conexión online">`
+  estado.innerHTML = `<img class="gif" src="../assets/offlinefinal.gif" alt="circulo verde representando conexión online">`
 
 } else {
-  estado.innerHTML = `<img class="gif" src="assets/onlinefinal.gif" alt="circulo verde representando conexión online">`
+  estado.innerHTML = `<img class="gif" src="../assets/onlinefinal.gif" alt="circulo verde representando conexión online">`
 
 }
 
